876e592f76e86bbf6b0edc2abdfb478bea4aa8b6
trueeric <ikpassat@gmail.com>
2024-08-13 21:23:05 +0800

    106. Defining and Creating Store

---
+++ src/stores/recipe.ts
@@ -0,0 +1,16 @@
+import { defineStore } from "pinia";
+import { ref } from "vue";
+
+interface Recipe {
+  id:string
+  name:string
+  description: string
+}
+
+export const useRecipeStore =
+  defineStore("recipe",()=> {
+    const recipes=ref<Recipe[]>([])
+
+    return {recipes}
+  })
+
